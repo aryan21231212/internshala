aee1a16327b23d1d8cae9251f1bd071e16af440a
aryanuu21 <aryanpratapsingh674@gmail.com>
2025-07-07 18:27:52 +0530

    changes made

--- backend/index.js
+++ backend/index.js
@@ -6,30 +6,30 @@ const { connect } = require("./db");
 const router = require("./Routes/index");
 const  { GoogleGenAI } =  require("@google/genai") ;
 
-// const ai = new GoogleGenAI({});
+
 
 const port = 5000;
 
-// Database connection
+
 connect();
 
-// Middleware
+
 app.use(cors());
 app.use(bodyParser.json({ limit: "50mb" }));
 app.use(bodyParser.urlencoded({ extended: true, limit: "50mb" }));
 app.use(express.json());
 
-// Routes
+
 app.get("/", (req, res) => {
   res.send("Internshala Backend Service");
 });
 
 app.use("/api", router);
 
-// Initialize Google Generative AI
+
 const ai = new GoogleGenAI({ apiKey: "AIzaSyCL52XFhgGUm6DaB9JpMi7VO2tBdv7SB20" });
 
-// Strict internship-focused chatbot
+
 app.post("/chat", async (req, res) => {
   let {question} = req.body;
   const response = await ai.models.generateContent({
